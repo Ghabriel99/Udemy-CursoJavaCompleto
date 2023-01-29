2dede1a4325cb53ff6a81557540d2f3fd7d8fe1c
Ghabriel Marchesi Schiavo <86672683+Ghabriel99@users.noreply.github.com>
2023-01-29 11:11:05 -0300

    .
    First commit

---
+++ CursoJava/src/Exercicios/exs.java
@@ -0,0 +1,17 @@
+package Exercicios;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class exs {
+   public static void main(String[] args) {
+      Locale.setDefault(Locale.US);
+      
+      Scanner sc = new Scanner(System.in);
+
+      System.out.println("Enter the rectangle width and height");
+
+      sc.close();
+
+
+   }
+}
