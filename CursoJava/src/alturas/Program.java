1fbe0f4971acdf6ea8d9060a2d68b8e7b0a14d02
Ghabriel Marchesi Schiavo <86672683+Ghabriel99@users.noreply.github.com>
2023-01-31 20:55:59 -0300

    .
    .

---
+++ CursoJava/src/alturas/Program.java
@@ -0,0 +1,28 @@
+package alturas;
+
+import java.util.Scanner;
+
+public class Program {
+   public static void main(String[] args) {
+      
+      Scanner sc = new Scanner(System.in);
+
+      int n = sc.nextInt();
+      double[] vect = new double[n];
+
+      for(int i =0; i<n; i++) {
+         vect[i] = sc.nextDouble();
+      }
+
+      double sum = 0;
+      for(int i=0; i<n; i++){
+         sum += vect[i];
+      }
+
+      double avg = sum / n;
+
+      System.out.printf("AVAREGE HEIGHT: %.2f%n" , avg);
+
+      sc.close();
+   }
+}
